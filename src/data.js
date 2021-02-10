79f27aebea00296a767d8aca13d8dfea62532139
dilshimenik25 <dilshimanik95@gmail.com>
2021-02-10 14:50:45 +0530

    second commit

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export default {
+products:[
+    {
+    name:'Cherries Casual Shoes',
+    Category:'Shoes',
+    image:'/images/img1.jpg',
+    price:70.00,
+    brand:'Nike',
+    rating:4.5,
+    numReviews: 10
+},
+{
+    name:'Cherries Casual Shoes',
+    Category:'Shoes',
+    image:'/images/img2.jpg',
+    price:90.00,
+    brand:'Nike',
+    rating:4.9,
+    numReviews: 4
+},
+{
+    name:'Cherries Casual Shoes',
+    Category:'Shoes',
+    image:'/images/img3.jpg',
+    price:40.00,
+    brand:'Nike',
+    rating:4.0,
+    numReviews: 7
+},
+{
+    name:'Cherries Casual Shoes',
+    Category:'Shoes',
+    image:'/images/img4.jpg',
+    price:80.00,
+    brand:'Nike',
+    rating:3.5,
+    numReviews: 10
+},
+{
+    name:'Cherries Casual Shoes',
+    Category:'Shoes',
+    image:'/images/img5.jpg',
+    price:75.00,
+    brand:'Nike',
+    rating:5.0,
+    numReviews: 8
+},
+{
+    name:'Cherries Casual Shoes',
+    Category:'Shoes',
+    image:'/images/img6.jpg',
+    price:79.00,
+    brand:'Nike',
+    rating:4.2,
+    numReviews: 4
+},
+
+]
+}
